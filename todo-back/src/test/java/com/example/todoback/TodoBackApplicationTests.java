4d987bb7481a1bca4cd7e29f077f5db91fa338a8
seonghun120614 <seonghun120614@gmail.com>
2025-11-27 11:20:56 +0900

    chore: 프로젝트 세팅 및  세팅

---
+++ todo-back/src/test/java/com/example/todoback/TodoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
